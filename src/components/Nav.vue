96ae0183264df48be0cdd360360c53eef1599cf0
BlueYang <2931442305@qq.com>
2021-12-24 17:48:36 +0800

    添加 Nav 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
-      <Icon name="money"/>
-      记账
-    </router-link>
-    |
-    <router-link to="/labels">
+  <nav>
+    <router-link to="/labels" class="item">
       <Icon name="label"/>
       标签
     </router-link>
-    |
-    <router-link to="/statistics">
+    <router-link to="/money" class="item">
+      <Icon name="money"/>
+      记账
+    </router-link>
+    <router-link to="/statistics" class="item">
       <Icon name="statistics"/>
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -27,5 +25,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
+nav {
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+  font-size: 12px;
+
+  .item {
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 3px 0;
+    .icon{
+      width: 32px;
+      height: 32px;
+    }
+  }
+}
 
 </style>
