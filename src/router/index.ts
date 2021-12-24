8f797bfb58375acfdb34824dbda7838435df4b45
BlueYang <2931442305@qq.com>
2021-12-24 09:38:25 +0800

    添加 404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,37 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '@/views/Home.vue'
-import Money from '@/views/Money.vue'
-import Labels from '@/views/Labels.vue'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   //默认路径进入'/money'
   {
-    path:'/',
-    redirect:'/money'
+    path: '/',
+    redirect: '/money'
   },
   //记账
   {
-    path:'/money',
-    component:Money,
+    path: '/money',
+    component: Money,
   },
   //标签
   {
-    path:'/labels',
-    component:Labels,
+    path: '/labels',
+    component: Labels,
   },
   //统计
   {
-    path:'/statistics',
-    component:Statistics
+    path: '/statistics',
+    component: Statistics
+  },
+  //404界面，当上边所有的路由走不通才会走通配符
+  {
+    path: '*',
+    component: NotFound
   }
   // {
   //   path: '/',
@@ -41,10 +46,10 @@ const routes: Array<RouteConfig> = [
   //   // which is lazy-loaded when the route is visited.
   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   // }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>404 NotFound</h1>
+    <br>
+    当前页面不存在，请重新输入
+    <br>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
